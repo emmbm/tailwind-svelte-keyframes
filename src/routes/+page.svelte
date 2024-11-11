32feb6cbb7658fe80a71c1323700a0afa729fb4a
Emmanuel Beaudry Marchand <emmbema@gmail.com>
2024-11-11 14:58:38 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<p class="without-prefix">This should animate using global keyframes defined within the component using the prescribed <code>-global-</code> prefix.</p>
+<p class="with-prefix">This should <b>not</b> animate as the keyframes' name should be stripped of the <code>-global-</code> prefix.</p>
+
+<style>
+  @keyframes -global-test {
+    0%,100% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 100%;
+    }
+  }
+
+  .without-prefix {
+    animation: 2s ease-in-out 0s infinite test;
+  }
+
+  .with-prefix {
+    animation: 2s ease-in-out 0s infinite -global-test;
+  }
+</style>
